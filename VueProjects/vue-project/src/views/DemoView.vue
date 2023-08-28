90077e596dbeb6233fdf177e9e3652312d5ac797
Sam Cairns <s222492079@deakin.edu.au>
2023-08-28 15:52:00 +1000

    SIT120 Tasks

---
+++ VueProjects/vue-project/src/views/DemoView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const itemID = ref(0);
+const item = ref(' ');
+const itemlist = ref([]);
+const check = ref(false);
+
+function addItem() {
+    itemlist.value.push({
+        id: itemID.value++,
+        newItem: item.value,
+        status: check.value
+    })
+
+}
+
+function removeitem(itemtoremove){
+    itemlist.value = itemlist.value.filter(xx => xx.id !== itemtoremove.id)
+}
+</script>
+
+<template>
+    <div>
+        <h1>This is a Demo Page</h1>
+        <input type="text" v-model="item">
+        <button @click=addItem> Add! </button>
+        <ol>
+            <li v-for="i in itemlist" :key="i.id">
+            {{ i.newItem }}
+            <button @click= "removeitem(i)"> Clear </button>
+            </li>
+        </ol>
+    </div>
+</template>
+
+
+    <style scoped>
+
+    </style>
---
+++ VueProjects/vue-project/src/views/MyComponent.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import { ref } from 'vue';
+
+const message= ref("Hello! This message if from a component!")
+
+const changeMessage = () => {
+    message.value = 'Updated Message!';
+};
+</script>
+
+<template>
+    <div>
+        <h2> {{ message }}</h2>
+        <button @click="changeMessage"> Change Message! </button>
+    </div>
+</template>
